0ac5da01a2086df3829361c7294162a262cd37f7
Administrator <huffiewang@gmail.com>
2020-09-18 14:33:24 +0800

    objectbox代理类接口

---
+++ base/src/main/java/com/musheng/android/common/objectbox/XTranslate.java
@@ -0,0 +1,10 @@
+package com.musheng.android.common.objectbox;
+
+/**
+ * Author      : MuSheng
+ * CreateDate  : 2020/9/18 14:15
+ * Description :
+ */
+public interface XTranslate {
+    Object translate(Object object);
+}
---
+++ base/src/main/java/com/musheng/android/common/objectbox/XTranslation.java
@@ -0,0 +1,10 @@
+package com.musheng.android.common.objectbox;
+
+/**
+ * Author      : MuSheng
+ * CreateDate  : 2020/9/17 11:46
+ * Description :
+ */
+public interface XTranslation<T> {
+    T translate();
+}
